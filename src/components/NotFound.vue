a0d6bc7b67a1f84fc821785bf14cfc162b2146e8
simonestaffa <simonestaffa96@gmail.com>
2020-04-04 20:58:49 +0200

    404 not found handled

---
+++ src/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="NotFound container text-center mt-5">
+        <h1>Errore 404! <br> Pagina non trovata :(</h1>
+        <img class="mt-5" src="../assets/404.svg" width="250">
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "NotFound"
+  }
+</script>
+
+<style scoped>
+
+</style>
