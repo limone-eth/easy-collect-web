7b373c1a077b1b15e454236759062d6cf390a97e
simonestaffa <simonestaffa96@gmail.com>
2020-04-04 12:13:28 +0200

    New partner links

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="row mt-5">
-        <div class="col-4"></div>
+        <div class="col-4">
+            <a href="http://lapecoranera.net"><img src="../assets/logo_PNE.png"
+                                                            alt="HostingSolutions" width="175"></a>
+        </div>
         <div class="col-4">
             <a href="https://www.hostingsolutions.it/"><img src="../assets/logo-hostingsolutions.png"
                                                             alt="HostingSolutions" width="175"></a>
         </div>
-        <div class="col-4"></div>
+        <div class="col-4">
+            <a href="https://passionfruithub.com/"><img src="../assets/logo-passion-fruit.jpg"
+                                                            alt="HostingSolutions" width="175"></a>
+        </div>
     </div>
 </template>
 
