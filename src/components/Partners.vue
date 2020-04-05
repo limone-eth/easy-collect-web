5f16c25748ed60c4d8e4dbe8ec855d3a9005f405
simonestaffa <simonestaffa96@gmail.com>
2020-04-05 10:34:18 +0200

    Fix responsiveness who we are

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,17 +1,31 @@
 <template>
     <div class="row mt-5">
-        <div class="col-4">
-            <a href="http://lapecoranera.net"><img src="../assets/logo_PNE.png"
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <a href="http://lapecoranera.net"><img src="../assets/logo_PNE.png"
+                                                       alt="HostingSolutions" width="175"></a>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 pt-3">
+                <a href="https://www.hostingsolutions.it/"><img src="../assets/logo-hostingsolutions.png"
+                                                                alt="HostingSolutions" width="175"></a>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 pt-3">
+                <a href="https://passionfruithub.com/"><img src="../assets/logo-passion-fruit.jpg"
                                                             alt="HostingSolutions" width="175"></a>
-        </div>
-        <div class="col-4">
-            <a href="https://www.hostingsolutions.it/"><img src="../assets/logo-hostingsolutions.png"
+            </div>
+        <!--<div class="container d-block d-sm-none">
+            <div class="col-12">
+                <a href="http://lapecoranera.net"><img src="../assets/logo_PNE.png"
+                                                       alt="HostingSolutions" width="175"></a>
+            </div>
+            <div class="col-12">
+                <a href="https://www.hostingsolutions.it/"><img src="../assets/logo-hostingsolutions.png"
+                                                                alt="HostingSolutions" width="175"></a>
+            </div>
+            <div class="col-12">
+                <a href="https://passionfruithub.com/"><img src="../assets/logo-passion-fruit.jpg"
                                                             alt="HostingSolutions" width="175"></a>
-        </div>
-        <div class="col-4">
-            <a href="https://passionfruithub.com/"><img src="../assets/logo-passion-fruit.jpg"
-                                                            alt="HostingSolutions" width="175"></a>
-        </div>
+            </div>
+        </div>-->
     </div>
 </template>
 
