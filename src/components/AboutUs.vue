b2f046d3cc7c8388c96a2e6459817b952c649e9d
simonestaffa <simonestaffa96@gmail.com>
2020-04-16 19:36:29 +0200

    About us

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -57,46 +57,46 @@
                 <div class="col-12 text-left align-self-center">
                     <div class="h2">Parlano di noi</div>
                 </div>
-                <div class="col-12">
+                <div class="col-12 ml-5">
                     <div class="row mt-4">
-                        <div class="col-6 pt-4">
+                        <div class="col-12">
                             <a href="https://www.mangiaebevi.it/nasce-colligo-lapp-per-fare-la-spesa-nelle-botteghe-di-quartiere-con-un-click/" target="_blank">
                                 <img src="../assets/about-us/mangiaebevi.png" alt="Mangia e Bevi" width="200">
                             </a>
                         </div>
-                        <div class="col-6 pb-5">
+                        <div class="col-12">
                             <a href="https://www.corriere.it/pianeta2020/20_aprile_11/niente-piu-file-market-ci-pensa-pizzicagnolo-40-rivincita-botteghe-quartiere-bcf308dc-7b53-11ea-8187-c429688ed758.shtml" target="_blank">
                                 <img src="../assets/about-us/Corriere_della_Sera.png" alt="Corriere della sera" width="200">
                             </a>
                         </div>
-                        <div class="col-6 pb-5">
+                        <div class="col-12">
                             <a href="https://www.ilmessaggero.it/roma/news/roma_coronavirus_spesa_botteghe_artigianali_piattaforma_colligo-5168439.html" target="_blank">
                                 <img src="../assets/about-us/il-messaggero.jpeg" alt="Messaggero" width="200">
                             </a>
                         </div>
-                        <div class="col-6 pt-2">
+                        <div class="col-12">
                             <a href="http://www.ilpuntonews.net/covid-19-colligo-la-spesa-in-bottega-comodamente-da-casa/" target="_blank">
                                 <img src="../assets/about-us/ilpuntonews.png" alt="Punto news" width="180">
                             </a>
                         </div>
                     </div>
                     <div class="row mt-4">
-                        <div class="col-6">
+                        <div class="col-12">
                             <a href="https://it.mashable.com/coronavirus/2737/nasce-colligo-app-per-le-botteghe-di-quartiere" target="_blank">
                                 <img src="../assets/about-us/mashable2.png" alt="Mashable" width="200">
                             </a>
                         </div>
-                        <div class="col-6">
+                        <div class="col-12">
                             <a href="http://www.comunicati-stampa.net/com/arriva-colligo-la-soluzione-per-fare-la-spesa-in-bottega-dal-cellulare-e-ritirarla-in-sicurezza-senza-file.html" target="_blank">
                                 <img src="../assets/about-us/comunicati-stampa.png" alt="Comunicati stampa" width="200">
                             </a>
                         </div>
-                        <div class="col-6">
+                        <div class="col-12">
                             <a href="https://it.geosnews.com/p/it/lazio/rm/roma/coronavirus-le-botteghe-diventano-hi-tech-la-spesa-si-fa-con-un-clic-consegna-a-domicilio_28939394" target="_blank">
                                 <img src="../assets/about-us/geosnews.png" alt="Geos news" width="200">
                             </a>
                         </div>
-                        <div class="col-6">
+                        <div class="col-12">
                             <a href="https://www.fattitaliani.it/2020/04/colligo-per-mettere-in-comunicazione.html" target="_blank">
                                 <img src="../assets/about-us/logo-Fattitaliani-p0.jpg" alt="Fatti italiani" width="200">
                             </a>
