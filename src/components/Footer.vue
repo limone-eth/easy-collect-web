79036ef4fd552928ff7ef766f1cab5f565613265
Halpph <cesaretti.1712254@studenti.uniroma1.it>
2020-04-16 18:29:08 +0200

    things added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,6 +4,12 @@
             <div class="pr-3">
                 <a href="https://fb.me/ColliGo.Italia" target="_blank"><img src="../assets/facebook.svg" width="50"></a>
             </div>
+
+            <div >
+                <a href="https://play.google.com/store/apps/details?id=com.easycollect.webapp" target="_blank"><img src="../assets/1280px-Google_Play_Store_badge_EN.svg.png"
+                                                                                                                    width="170" alt="PlayStore"></a>
+            </div>
+
             <div class="pl-3">
                 <a href="https://instagram.com/colligoitalia?igshid=1jo7dpg2rdaig" target="_blank"><img src="../assets/instagram.svg"
                                                                                         width="50" alt="Instagram"></a>
@@ -11,6 +17,9 @@
         </div>
         <div class="text-center mt-4">
             <a href="mailto:colligo.shop@gmail.com">colligo.shop@gmail.com</a>
+        </div>
+        <div class="text-center mt-3">
+            <a href="/policy" class="text-dark">Policy</a>
         </div>
     </div>
 </template>
